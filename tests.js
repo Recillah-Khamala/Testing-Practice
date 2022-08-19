d51de063deaf4ff2ba9dbb3d72ec850ccbad1ce8
Recillah Khamala <recillahk@gmail.com>
2022-08-19 06:36:00 +0300

    install jest

--- tests.js
+++ tests.js
@@ -1,28 +1,39 @@
-import Calculator from "./calculator";
-import { stringLength, reverseString, capitalize } from "./tasks";
+const Calculator = require("./calculator");
+const {
+    stringLength,
+    reverseString,
+    capitalize
+} = require("./tasks");
 
-// tests for task 1 string length
-test('function to return the total number of characters in a string', () => {
-    expect(stringLength('come here')).tobe(8)
+describe('stringLength', () => {
+    // tests for task 1 string length
+    it('returns the total number of characters in a string', () => {
+        expect(stringLength('come here')).toEqual(8)
+    })
+
+    it('throws an error as a string has to have more than one character', () => {
+        expect(() => stringLength('')).toThrowError('Should have atleast one character')
+    })
+
+    it('it throws an error as a string has to have less than ten character', () => {
+        expect(() => stringLength('')).toThrorError('Should have less than 10 characters')
+    })
 })
 
-test('a string to have more than one character', () => {
-    expect(() => stringLength('')).tothrow('Should have atleast one character')
+describe('reverseString', () => {
+    it('reverses the string', () => {
+        expect(reverseString('hello')).toBe('olleh')
+    })
+
+    it('retrurns an empty string', () => {
+        expect(reverseString('')).toBe('')
+    })
 })
 
-test('a string to have less than ten character', () => {
-    expect(() => stringLength('')).tothrow('Should have less than 10 characters')
-})
-
-
-// test for task 2 reverse string
-
-test('function to reverse the string', () => {
-    expect(reverseString('hello')).tobe('olleh')
-})
-
-test('function to retrurn an empty string', () => {
-    expect(reverseString('')).tobe('')
+describe('capitalize', () => {
+    it('capitalizes the first character', () => {
+        expect(capitalize('neema')).toBe('Neema')
+    })
 })
 
 // tests for task 3 calculator
@@ -30,55 +41,49 @@ test('function to retrurn an empty string', () => {
 const calculator = new Calculator();
 
 describe('Addition', () => {
-    test('when you add two numbers, the output will be their correct summation', () => {
-        expect(() => calculator.add(3, 6)).tobe(9);
+    it('sums up two numbers', () => {
+        expect(() => calculator.add(3, 6)).toEqual(9);
     })
-    test('when negative numbers are added, the correct output is given', () => {
-        expect(() => calculator.add(-4, -1)).tobe(-5);
+    it('sums up negative numbers', () => {
+        expect(() => calculator.add(-4, -1)).toEqual(-5);
     })
-    test('When numbers with decimal points are added, the correct output s given', () => {
-        expect(() => calculator.add(4.3, 2.5)).tobe(6.8);
+    it('sums up numbers with decimal points', () => {
+        expect(() => calculator.add(4.3, 2.5)).toEqual(6.8);
     })
 })
 
 describe('Subtraction', () => {
-    test('correct output when subtraction two numbers', () => {
-        expect(() => calculator.subtract(9, 1)).tobe(8);
+    it('gives correct output when subtraction two numbers', () => {
+        expect(() => calculator.subtract(9, 1)).toEqual(8);
     })
-    test('correct output when subtraction two negative numbers', () => {
-        expect(() => calculator.subtract(-6, -1)).tobe(-5);
+    it('gives correct output when subtraction two negative numbers', () => {
+        expect(() => calculator.subtract(-6, -1)).toEqual(-5);
     })
-    test('correct output when subtraction two numbers with decimal points', () => {
-        expect(() => calculator.subtract(9.6, 1.8)).tobe(7.8);
+    it('gives correct output when subtraction two numbers with decimal points', () => {
+        expect(() => calculator.subtract(9.6, 1.8)).toEqual(7.8);
     })
 })
 
 describe('division', () => {
-    test('correst output when dividing two numbers', () => {
-        expect(() => calculator.divide(8,4)).tobe(2);
+    it('gives correst output when dividing two numbers', () => {
+        expect(() => calculator.divide(8, 4)).toEqual(2);
     });
-    test('correst output when dividing two numbers where one of them is 0', () => {
-        expect(() => calculator.divide(0,3)).tobe(0);
+    it('gives correst output when dividing two numbers where one of them is 0', () => {
+        expect(() => calculator.divide(0, 3)).toEqual(0);
     });
-    test('correst output when dividing two numbers with decimal points', () => {
-        expect(() => calculator.divide(0.4,0.2)).tobe(0.2);
+    it('gives correst output when dividing two numbers with decimal points', () => {
+        expect(() => calculator.divide(0.4, 0.2)).toEqual(0.2);
     });
 })
 
 describe('multiplication', () => {
-    test('correct output for multiplying 2 numbers', () => {
-        expect(() => calculator.multiply(2,5)).tobe(10);
+    it('gives correct output for multiplying 2 numbers', () => {
+        expect(() => calculator.multiply(2, 5)).toEqual(10);
     });
-    test('correct output for multiplying 2 numbers with one being 0', () => {
-        expect(() => calculator.multiply(6,0)).tobe(0);
+    it('gives correct output for multiplying 2 numbers with one being 0', () => {
+        expect(() => calculator.multiply(6, 0)).toEqual(0);
     });
-    test('correct output for multiplying 2 numbers with decimal points', () => {
-        expect(() => calculator.multiply(2.3,5.1)).tobe(11.73);
+    it('gives correct output for multiplying 2 numbers with decimal points', () => {
+        expect(() => calculator.multiply(2.3, 5.1)).toEqual(11.73);
     });
-})
-
-
-// tests for task 4 capitalize
-test('first character should be capitalized', () => {
-    expect(capitalize('neema')).tobe('Neema')
 })
